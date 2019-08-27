2248e778e69ce21a6995206fb6533b8d0685e4cd
Benjamin Nguyen <43457281+benjamonnguyen@users.noreply.github.com>
2019-08-27 14:30:46 -0700

    heres my stuff

--- pages_Ben/assets/js/renderEF.js
+++ pages_Ben/assets/js/renderEF.js
@@ -957,8 +957,8 @@ modPfolio = toMod.filter(function(value){
 });
 
 // RENDER CHART!
-function renderChart() {
-    var newChart = function newChart(chart, config) {
+function renderEF() {
+    function newChart(chart, config) {
         var ctx = chart.getContext('2d');
         return new window.Chart(ctx, config);
     };
@@ -1102,6 +1102,7 @@ function renderChart() {
                     return value.id != 'bad';
                 });
                 scatterChart.data.datasets = filtered;
+                console.log(scatterChart.data.datasets);
                 scatterChart.update();
             }
             else {
@@ -1133,4 +1134,4 @@ function off() {
 function on() {
     document.getElementById('overlay').style.display = 'block';
 }
-window.onload=renderChart();
+window.onload=renderEF();
